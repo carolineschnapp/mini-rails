d9e934bd6229b88c375a52c9c07b9bb0c0ce0aa1
Caroline <mllegeorgesand@gmail.com>
2017-02-22 22:03:55 -0500

    Refactor to work with any model / table

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -2,43 +2,59 @@ require 'active_record/connection_adapter'
 
 module ActiveRecord
   class Base
-    def self.abstract_class=(value)
-      # Not implemented
+    class << self
+      def class_name
+        name.downcase + 's'
+      end
+
+      def all
+        find_by_sql("SELECT * from #{class_name}")
+      end
+
+      def find(id)
+        find_by_sql("SELECT * from #{class_name} WHERE id = #{id.to_i}").first
+      end
+
+      def establish_connection(options)
+        @@connection = ConnectionAdapter::SqliteAdapter.new(options[:database])
+      end
+
+      def connection
+        @@connection
+      end
+
+      def find_by_sql(sql)
+        connection
+          .execute(sql)
+          .map do |attributes|
+            new(attributes)
+          end
+      end
+
+      def find_by_sql(sql)
+        connection
+          .execute(sql)
+          .map do |attributes|
+            new(attributes)
+          end
+      end
+
+      def abstract_class=(value)
+        # Not implemented
+      end
     end
 
     def initialize(attributes = {})
       @attributes = attributes
     end
 
-    def id
-      @attributes[:id]
-    end
-
-    def title
-      @attributes[:title]
-    end
-
-    def self.all
-      connection
-        .execute("SELECT * from posts")
-        .map do |attributes|
-          new(attributes)
-        end
-    end
-
-    def self.find(id)
-      attributes = connection
-        .execute("SELECT * from posts WHERE id = #{id.to_i}")
-        .first
-      new(attributes)
-    end
-
-    def self.establish_connection(options)
-      @@connection = ConnectionAdapter::SqliteAdapter.new(options[:database])
-    end
-
-    def self.connection
-      @@connection
+    def method_missing(name, *args)
+      columns = self.class.connection.columns(self.class.class_name)
+      if columns.include?(name)
+        @attributes[name]
+      else
+        super
+      end
     end
   end
 end
